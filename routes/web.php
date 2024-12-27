7c23297efe0fbb0f2684b2c99b1bcbc3c560899d
unknown <masuduciha@gmail.com>
2024-12-27 21:38:09 +0700

    study h-1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/blogg', function () {
+//   return 'Hello World';
+// });  -> basic route
+
+// Route::view('/blog', 'blog'); -> route view 
+
+Route::get('/blog', function () {
+    return view('blog');
+}); // -> route view dengan method
+
+Route::get('/blog/{id}', function (Request $request) {
+    return 'this is blog '.$request->route('id');
+}); // -> route parameter
